1a62fd74279850eea2ecbe71d58a52f9c5cfe783
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-04-06 15:30:01 +0200

    Setup iniziale

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/*
+Chiedere km da percorrere.
+Chiedere età.
+Calcola prezzo in base a distanza.
+Calcola sconto minorenni (se applicabile).
+Calcola sconto anziani (se applicabile).
+Produci l'autput con due decimali.
+*/
+
+console.log('hello world');
